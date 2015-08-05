b322af552d1df4cf045853e28ad1ee3768b70c79
Daniel Bugl <daniel.bugl@touchlay.com>
2015-08-05 12:32:18 -0400

    0.2.3: small design improvement :)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 Logs = new Mongo.Collection("logs");
 
-VERSION = "0.2.2";
+VERSION = "0.2.3";
 
 LIMIT = (Meteor.settings && Meteor.settings.public) ? Meteor.settings.public.limit : 0;
 LIMIT = (LIMIT > 0) ? LIMIT : 25;
